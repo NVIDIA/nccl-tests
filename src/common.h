064cbede7943b9cfd70f4dc3f745f70441890751
Katie Gioioso <kgioioso@nvidia.com>
2025-12-30 20:16:39 +0000

    NCCL_TESTS_VERSION 2.17.7

--- src/common.h
+++ src/common.h
@@ -6,7 +6,7 @@
 #ifndef __COMMON_H__
 #define __COMMON_H__
 
-#define NCCL_TESTS_VERSION "2.17.6"
+#define NCCL_TESTS_VERSION "2.17.7"
 
 #include "nccl.h"
 #if NCCL_VERSION_CODE >= NCCL_VERSION(2,28,0)
