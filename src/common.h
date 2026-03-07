dd0bafd17891809734e1bbbd6c2a3a778f3ff63e
David Addison <daddison@nvidia.com>
2026-03-06 17:55:12 -0800

    NCCL_TESTS_VERSION 2.18.0

--- src/common.h
+++ src/common.h
@@ -6,7 +6,7 @@
 #ifndef __COMMON_H__
 #define __COMMON_H__
 
-#define NCCL_TESTS_VERSION "2.17.10"
+#define NCCL_TESTS_VERSION "2.18.0"
 
 #include "nccl.h"
 #if NCCL_VERSION_CODE >= NCCL_VERSION(2,28,0)
